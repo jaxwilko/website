bec8beefe1b3c613b52485c621e01b5811a334a4
Jack Wilkinson <me@jackwilky.com>
2020-11-16 17:14:42 +0000

    Added 50/50 chance to draw dots or cubes in the scene

--- src/core/scene.js
+++ src/core/scene.js
@@ -1,4 +1,5 @@
 import Cube from "../entities/cube";
+import Dot from "../entities/dot"
 import Vector from "./vector";
 import Velocity from "./velocity";
 import Random from "./random";
@@ -10,19 +11,33 @@ export default class Scene
     }
 
     getEntities(engine) {
-        let cubes = [];
+        let entities = [];
 
-        for (let i = 0; i < 90; i++) {
-            cubes.push(
-                new Cube(
-                    new Vector(this.random.number(0, engine.canvas.getWidth()), this.random.number(0, engine.canvas.getHeight())),
-                    new Velocity(this.random.number(-5, 5), this.random.number(-5, 5)),
-                    13,
-                    this.random.colour()
-                )
-            );
+        if (this.random.chance()) {
+            for (let i = 0; i < 90; i++) {
+                entities.push(
+                    new Cube(
+                        new Vector(this.random.number(0, engine.canvas.getWidth()), this.random.number(0, engine.canvas.getHeight())),
+                        new Velocity(this.random.number(-5, 5), this.random.number(-5, 5)),
+                        13,
+                        this.random.colour()
+                    )
+                );
+            }
+        } else {
+            for (let i = 0; i < 170; i++) {
+                entities.push(
+                    new Dot(
+                        new Vector(this.random.number(0, engine.canvas.getWidth()), this.random.number(0, engine.canvas.getHeight())),
+                        new Velocity(this.random.number(-5, 5), this.random.number(-5, 5)),
+                        5,
+                        this.random.colour(),
+                        false
+                    )
+                );
+            }
         }
 
-        return cubes;
+        return entities;
     }
 }
